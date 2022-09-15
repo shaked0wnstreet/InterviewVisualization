198d542c2544400009d4cf151785c35776fef520
Matt Fadler <mfadler@Matts-MacBook-Pro.local>
2022-09-15 09:35:46 -0500

    connect react to flask api

--- interview-visualization/src/App.js
+++ interview-visualization/src/App.js
@@ -1,13 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [flaskData, setFlaskData] = useState();
+
+  useEffect(() => {
+    fetch('/message').then(res => res.json()).then(data => {
+      setFlaskData(data.message);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World.
+          {flaskData}
         </p>
       </header>
     </div>
