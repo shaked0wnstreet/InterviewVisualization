68d95cb67548a5ca3909ce729d433912bd305882
Emily (Thi) Nguyen <emily.tn.98@gmail.com>
2022-10-19 15:58:42 -0500

    Code for the pop up form

--- interview-visualization/src/App.js
+++ interview-visualization/src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import PopUpForm  from './component/PopUpForm';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+
 
 function App() {
+  const [onPopUp, setOnPopUp] = useState(false); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World!
-        </p>
-      </header>
+      <main>
+        <h1>React popups</h1>
+        <br></br>
+        <Button variant="contained" onClick={() => setOnPopUp(true)}>Open PopUp</Button>
+        {onPopUp ? <PopUpForm setTrigger={setOnPopUp}/> : ''}
+      </main>
     </div>
   );
 }
