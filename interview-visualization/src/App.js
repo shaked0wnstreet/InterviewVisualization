b56e872106ca1efeacf61c9f2eafbff2d45ef5a4
Emily (Thi) Nguyen <emily.tn.98@gmail.com>
2022-10-19 16:08:36 -0500

    Revert "Code for the pop up form"
    This reverts commit 68d95cb67548a5ca3909ce729d433912bd305882.

--- interview-visualization/src/App.js
+++ interview-visualization/src/App.js
@@ -1,19 +1,15 @@
+import logo from './logo.svg';
 import './App.css';
-import PopUpForm  from './component/PopUpForm';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
-
 
 function App() {
-  const [onPopUp, setOnPopUp] = useState(false); 
   return (
     <div className="App">
-      <main>
-        <h1>React popups</h1>
-        <br></br>
-        <Button variant="contained" onClick={() => setOnPopUp(true)}>Open PopUp</Button>
-        {onPopUp ? <PopUpForm setTrigger={setOnPopUp}/> : ''}
-      </main>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, World!
+        </p>
+      </header>
     </div>
   );
 }
