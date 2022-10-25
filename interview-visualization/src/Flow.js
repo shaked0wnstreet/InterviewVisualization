15ed84a41f4240387c7d4d9e10020c5080cc7756
ara5g <113125268+ara5g@users.noreply.github.com>
2022-10-25 08:27:10 -0500

    Update Flow.js
    Add work-in-progress loop that creates each interview node

--- interview-visualization/src/Flow.js
+++ interview-visualization/src/Flow.js
@@ -1,163 +1,92 @@
 import React, { useCallback } from "react";
 import ReactFlow, {
-    addEdge,
-    MiniMap,
-    Controls,
-    Background,
-    useNodesState,
-    useEdgesState
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState
 } from "reactflow";
 import "reactflow/dist/style.css";
 import { Text } from "@fluentui/react";
 
 import {
-    nodes as initialNodes,
-    edges as initialEdges
+  nodes as initialNodes,
+  edges as initialEdges
 } from "./initial-elements";
 
 import json from './GameDev.json';
 
-for (var i = 0; i < json["interviewerDialogs"].length; i++) {
+var newNodes = [];
 
-    var obj = json["interviewerDialogs"][i];
-    console.log(obj);
+for (var i=0; i < json["interviewerDialogs"].length; i++) {
+  var obj = {};
+  var position = {};
+  var data = {};
+
+  obj.id = String(Number(json["interviewerDialogs"][i]["id"].slice(-3)) + 1);
+  obj.type = "input";
+  obj.label = "Testing";
+  data.label = "Today, I am going to find out if your experience and interests will mesh well with our company, and in the process, you can learn more about our organization and the job.";
+  obj.data = data;
+  position.x =  "200";
+  position.y =  (i+1)*20;
+  obj.position = position;
+
+  newNodes.push(obj);
 }
 
-// const graphArray = [];
-
-// for (const key in json["interviewerDialogs"]) {
-//   if (json["interviewerDialogs"].hasOwnProperty(key)) {
-//     graphArray.push(key);
-//   }
-// }
-
-// console.log(graphArray);
-
-// graphArray.forEach(function(obj) {
-//   console.log(obj.id);
-// });
-
-
-
-// console.log(json);
+console.log(newNodes);
 
 const onInit = (reactFlowInstance) =>
-    console.log("flow loaded:", reactFlowInstance);
+  console.log("flow loaded:", reactFlowInstance);
 
 const OverviewFlow = () => {
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    const onConnect = useCallback(
-        (params) => setEdges((eds) => addEdge(params, eds)),
-        [setEdges]
-    );
-
-    const onNodeClick = () => {
-        return (
-            <Text>This is a tool tip for node 1</Text>
-        )
-    }
+  const [nodes, setNodes, onNodesChange] = useNodesState(newNodes);
+  // const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+  
+  const onNodeClick = () => {
     return (
-        <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            onConnect={onConnect}
-            onInit={onInit}
-            onNodeClick={onNodeClick}
-            fitView
-            attributionPosition="top-right"
-        >
-            <MiniMap
-                nodeStrokeColor={(n) => {
-                    if (n.style?.background) return n.style.background;
-                    if (n.type === "input") return "#0041d0";
-                    if (n.type === "output") return "#ff0072";
-                    if (n.type === "default") return "#1a192b";
+      <Text>This is a tool tip for node 1</Text>
+    )
+  }
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      onInit={onInit}
+      onNodeClick={onNodeClick}
+      fitView
+      attributionPosition="top-right"
+    >
+      <MiniMap
+        nodeStrokeColor={(n) => {
+          if (n.style?.background) return n.style.background;
+          if (n.type === "input") return "#0041d0";
+          if (n.type === "output") return "#ff0072";
+          if (n.type === "default") return "#1a192b";
 
-                    return "#eee";
-                }}
-                nodeColor={(n) => {
-                    if (n.style?.background) return n.style.background;
+          return "#eee";
+        }}
+        nodeColor={(n) => {
+          if (n.style?.background) return n.style.background;
 
-                    return "#fff";
-                }}
-                nodeBorderRadius={2}
-            />
-            <Controls />
-            <Background color="#aaa" gap={16} />
-        </ReactFlow>
-    );
+          return "#fff";
+        }}
+        nodeBorderRadius={2}
+      />
+      <Controls />
+      <Background color="#aaa" gap={16} />
+    </ReactFlow>
+  );
 };
 
 export default OverviewFlow;
-// import React, { useCallback } from "react";
-// import ReactFlow, {
-//   addEdge,
-//   MiniMap,
-//   Controls,
-//   Background,
-//   useNodesState,
-//   useEdgesState
-// } from "reactflow";
-// import "reactflow/dist/style.css";
-// import { Text } from "@fluentui/react";
-
-// import {
-//   nodes as initialNodes,
-//   edges as initialEdges
-// } from "./initial-elements";
-
-// const onInit = (reactFlowInstance) =>
-//   console.log("flow loaded:", reactFlowInstance);
-
-// const OverviewFlow = () => {
-//   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-//   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-//   const onConnect = useCallback(
-//     (params) => setEdges((eds) => addEdge(params, eds)),
-//     [setEdges]
-//   );
-
-//   const onNodeClick = () => {
-//     return (
-//       <Text>This is a tool tip for node 1</Text>
-//     )
-//   }
-//   return (
-//     <ReactFlow
-//       nodes={nodes}
-//       edges={edges}
-//       onNodesChange={onNodesChange}
-//       onEdgesChange={onEdgesChange}
-//       onConnect={onConnect}
-//       onInit={onInit}
-//       onNodeClick={onNodeClick}
-//       fitView
-//       attributionPosition="top-right"
-//     >
-//       <MiniMap
-//         nodeStrokeColor={(n) => {
-//           if (n.style?.background) return n.style.background;
-//           if (n.type === "input") return "#0041d0";
-//           if (n.type === "output") return "#ff0072";
-//           if (n.type === "default") return "#1a192b";
-
-//           return "#eee";
-//         }}
-//         nodeColor={(n) => {
-//           if (n.style?.background) return n.style.background;
-
-//           return "#fff";
-//         }}
-//         nodeBorderRadius={2}
-//       />
-//       <Controls />
-//       <Background color="#aaa" gap={16} />
-//     </ReactFlow>
-//   );
-// };
-
-// export default OverviewFlow;
-
