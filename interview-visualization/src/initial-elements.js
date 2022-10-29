dae39817475bd0c95efd4f518a7f23cb5a312645
Jonathan Garcia <garciajonathan2000@gmail.com>
2022-10-29 18:36:24 -0500

    initial-elements modification

--- interview-visualization/src/initial-elements.js
+++ interview-visualization/src/initial-elements.js
@@ -13,7 +13,7 @@ export const nodes = [
             ),
 
         },
-        position: { x: 250, y: 0 },
+        position: { x: 250, y: 15 },
         tooltip: {
             showTooltip: true,
             text: 'this is the tooltip for node1',
@@ -28,7 +28,7 @@ export const nodes = [
                 </>
             )
         },
-        position: { x: 100, y: 100 },
+        position: { x: 250, y: 115 },
 
     },
     {
@@ -40,7 +40,7 @@ export const nodes = [
                 </>
             )
         },
-        position: { x: 400, y: 100 },
+        position: { x: 500, y: 300 },
         style: {
             border: "1px solid #222138",
             width: 180
@@ -48,7 +48,7 @@ export const nodes = [
     },
     {
         id: "4",
-        position: { x: 190, y: 200 },
+        position: { x: 25, y: 225 },
         data: {
             label: (
                 <>
@@ -66,11 +66,10 @@ export const nodes = [
                 </>
             )
         },
-        position: { x: 20, y: 200 }
+        position: { x: 250, y: 400 }
     },
     {
         id: "6",
-        type: "output",
         data: {
             label: (
                 <>
@@ -78,13 +77,12 @@ export const nodes = [
                 </>
             )
         },
-        position: { x: 400, y: 450 }
+        position: { x: 250, y: 625 }
     },
     {
         id: "7",
-        type: "output",
         data: { label: "007" },
-        position: { x: 200, y: 450 }
+        position: { x: 25, y: 550 }
     },
     {
         id: "8",
@@ -95,47 +93,79 @@ export const nodes = [
                 </>
             )
         },
-        position: { x: 600, y: 250 }
+        position: { x: 500, y: 550 }
+    },
+    {
+        id: "9",
+        data: {
+            label: (
+                <>
+                    <strong>009</strong>
+                </>
+            )
+        },
+        position: { x: 500, y: 850 }
+    },
+    {
+        id: "10",
+        data: {
+            label: (
+                <>
+                    <strong>010</strong>
+                </>
+            )
+        },
+        position: { x: 20, y: 850 }
+    },
+    {
+        id: "11",
+        data: {
+            label: (
+                <>
+                    <strong>011</strong>
+                </>
+            )
+        },
+        position: { x: 250, y: 925 }
+    },
+    {
+        id: "12",
+        data: {
+            label: (
+                <>
+                    <strong>012</strong>
+                </>
+            )
+        },
+        position: { x: 250, y: 1150 }
     }
 ];
 
 export const edges = [
-    { id: "e1-2", source: "1", target: "2", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Sentiment: NO" },
-    { id: "e1-3", source: "1", target: "3", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Sentiment: YES" },
-    // { id: "e2-3", source: "2", target: "3", label: "this is an edge label" },
-    // { id: "e2-3", source: "2", target: "3", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Sentiment: YES" },
+    { id: "e1-2", source: "1", target: "2", type: 'straight', markerEnd: { type: MarkerType.Arrow } },
+    //{ id: "e1-3", source: "1", target: "3", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Sentiment: YES" },
     { id: "e2-4", source: "2", target: "4", type: 'straight', markerEnd: { type: MarkerType.Arrow } },
-    // { id: "e2-5", source: "2", target: "5", type: 'straight', markerEnd: { type: MarkerType.Arrow } },
+    { id: "e4-3", source: "4", target: "3", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Sentiment: YES" },
+    { id: "e3-5", source: "3", target: "5", type: 'straight', markerEnd: { type: MarkerType.Arrow } },
+    { id: "e4-5", source: "4", target: "5", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Sentiment: NO" },
     { id: "e5-7", source: "5", target: "7", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: LOW" },
+    { id: "e5-6", source: "5", target: "6", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: LOW" },
+    { id: "e5-8", source: "5", target: "8", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: LOW" },
+    { id: "e6-10", source: "6", target: "10", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: LOW" },
+    { id: "e6-11", source: "6", target: "11", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: LOW" },
+    { id: "e6-9", source: "6", target: "9", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: LOW" },
+    { id: "e7-10", source: "7", target: "10", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: HIGH" },
+    { id: "e7-11", source: "7", target: "11", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: HIGH" },
+    { id: "e7-9", source: "7", target: "9", type: 'straight', markerEnd: { type: MarkerType.Arrow } },
 
-    { id: "e3-8", source: "3", target: "8", type: 'straight', markerEnd: { type: MarkerType.Arrow } },
-    { id: "e8-6", source: "8", target: "6", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: HIGH" }
+    { id: "e8-10", source: "8", target: "10", type: 'straight', markerEnd: { type: MarkerType.Arrow } },
+    { id: "e8-11", source: "8", target: "11", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: HIGH" },
+    { id: "e8-9", source: "8", target: "9", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: LOW" },
+    { id: "e10-12", source: "10", target: "12", type: 'straight', markerEnd: { type: MarkerType.Arrow } },
+    { id: "e9-12", source: "9", target: "12", type: 'straight', markerEnd: { type: MarkerType.Arrow } },
+    { id: "e11-12", source: "11", target: "12", type: 'straight', markerEnd: { type: MarkerType.Arrow } }
 
 
-    // {
-    //   id: "e4-5",
-    //   source: "4",
-    //   target: "5",
-    //   label: "edge with arrow head",
-    //   markerEnd: {
-    //     type: MarkerType.ArrowClosed
-    //   }
-    // },
-    // {
-    //   id: "e5-6",
-    //   source: "5",
-    //   target: "6",
-    //   type: "smoothstep",
-    //   label: "smooth step edge"
-    // },
-    // {
-    //   id: "e5-7",
-    //   source: "5",
-    //   target: "7",
-    //   type: "step",
-    //   style: { stroke: "#f6ab6c" },
-    //   label: "a step edge",
-    //   animated: true,
-    //   labelStyle: { fill: "#f6ab6c", fontWeight: 700 }
-    // }
+    //{ id: "e3-8", source: "3", target: "8", type: 'straight', markerEnd: { type: MarkerType.Arrow } },
+    //{ id: "e8-6", source: "8", target: "6", type: 'straight', markerEnd: { type: MarkerType.Arrow }, label: "Stress: HIGH" }
 ];
