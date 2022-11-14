0cda0233acc45d3f20ca21ac5ea97c3776ec122b
ara5g <113125268+ara5g@users.noreply.github.com>
2022-11-14 10:05:32 -0600

    Added dynamically appearing a few nodes

--- interview-visualization/src/Flow.js
+++ interview-visualization/src/Flow.js
@@ -22,102 +22,76 @@ import {
 
 console.log(initialNodes);
 
-var newNodes = {
+var originalNodes = {
     "links":[],
     "nodes":[
         {
-            id: "1",
-            type: "input",
-            data: {
-              label: (
-                <>
-                  {` Good {{greetingTime}}, {{personName}}! Thank you so much for coming in. My name is {{interviewerName}}, and I am the supervisor for this department. I will be conducting this interview for the position of a Game Developer.  `}
-                </>
-              ),
-              
-            },
-            position: { x: 200, y: 0 },
-            tooltip: {
-              showTooltip: true,
-              text: 'this is the tooltip for node1',
-            },
-          },
-          {
-            id: "2",
-            data: {
-              label: (
-                <>
-                  Today, I am going to find out if your experience and interests will mesh well with our company, and in the process, you can learn more about our organization and the job.
-                </>
-              )
-            },
-            position: { x: 375, y: 0 },
-            
-          },
-          {
-            id: "3",
-            data: {
-              label: (
-                <>
-                  Before getting into some questions about the position, tell me, do you have any past work experience?
-                </>
-              )
-            },
-            position: { x: -50, y: 225 },
-            // style: {
-            //   background: "#D6D5E6",
-            //   color: "#333",
-            //   border: "1px solid #222138",
-            //   width: 180
-            // }
-          },
-          {
-            id: "4",
-            position: { x: -200, y: 350 },
-            data: {
-              label: "Great. What did you best like about the job?"
-            }
-          },
-          {
-            id: "5",
-            data: {
-              label: "What challenges did you face?"
-            },
-            position: { x: -250, y: 425 }
-          },
-          // {
-          //   id: "5",
-          //   data: {
-          //     label: "Let's start with some technical questions about the position that you are interviewing for. Do you have any experience using a game engine such as Unity or Unreal?"
-          //   },
-          //   position: { x: 250, y: 325 }
-          // },
-          {
-            id: "6",
-            data: {
-              label: (
-                <>
-                  Let's start with some technical questions about the position that you are interviewing for. Do you have any experience using a game engine such as Unity or Unreal?
-                </>
-              )
-            },
-            position: { x: 250, y: 325 }
-          },
-          {
-            id: "7",
-            data: { label: "That's okay. We can provide training in these tools." },
-            position: { x: 150, y: 500 }
-          },
-          {
-            id: "8",
-            data: { label: "Do you have experience with object-oriented programming languages such as C++, C#, or Java? If so, which ones have you used?" },
-            position: { x: 250, y: 725 }
-          },
-          {
-            id: "9",
-            data: { label: `Excellent. How would you rate your skill with \{\{developmentTool\}\}?` },
-            position: { x: 350, y: 500 }
-          },
+          "id": "000",
+          "DialogText": "Good {{greetingTime}}, {{personName}}! Thank you so much for coming in. My name is {{interviewerName}}, and I am the supervisor for this department. I will be conducting this interview for the position of a Game Developer. ",
+          "dynamicParams": [],
+          "staticParams": [
+              "interviewerName",
+              "greetingTime",
+              "personName"
+          ],
+          "alternates": [
+              "My name is {{interviewerName}}, and I am the supervisor for this department.",
+              "My name is {{interviewerName}}, and I am the department supervisor."
+          ],
+          "NextDialogID": "001",
+          "unrecognizedResponse": "I'm sorry, I don't understand.",
+          "requireResponse": false,
+          "userInterruptionEnabled": false,
+          "section": "Greeting"
+      },
+      {
+          "id": "001",
+          "DialogText": "Today, I am going to find out if your experience and interests will mesh well with our company, and in the process, you can learn more about our organization and the job. ",
+          "dynamicParams": [],
+          "staticParams": [],
+          "alternates": [
+              "I'd like to learn more about your background. Then I want to share information about our organization and the job. Shall we begin?",
+              "I want to learn more about you and talk about what we do here at our organization. Does that sound good?"
+          ],
+          "NextDialogID": "PastWork001",
+          "unrecognizedResponse": "I'm sorry, I don't understand.",
+          "timeLimit": 10,
+          "requireResponse": true,
+          "userInterruptionEnabled": false,
+          "section": "Greeting"
+      },
+      {
+          "id": "PastWork001",
+          "DialogText": "Before getting into some questions about the position, tell me, do you have any past work experience?",
+          "dynamicParams": [],
+          "staticParams": [],
+          "alternates": [
+              "Have you had any other jobs before this interview?",
+              "What other jobs have you worked before?"
+          ],
+          "filterType": "SentimentFilter",
+          "NextPostiveID": "PastWork002",
+          "NextNegativeID": "005",
+          "timeLimit": 30,
+          "requireResponse": true,
+          "nextTopicId": "005",
+          "section": "PreviousWorkExperience"
+      },
+      {
+          "id": "PastWork002",
+          "DialogText": "Great. What did you best like about the job?",
+          "dynamicParams": [],
+          "staticParams": [],
+          "alternates": [
+              "What did you learn from this experience?",
+              "What was one thing you learned from this job experience?"
+          ],
+          "NextDialogID": "PastWork003",
+          "timeLimit": 60,
+          "requiredParams": [],
+          "requireResponse": true,
+          "section": "PreviousWorkExperience"
+      },
     ]
 }
 
@@ -133,12 +107,21 @@ var newNodes = {
 //     ]
 // }
 
-// for (var i=0; i < newNodes["nodes"].length; i++) {
-//         newNodes["nodes"][i]["position"] = { x: 200, y: 0 + (i * 50) };
-//         console.log(newNodes["nodes"][i]);
-// }
+var newNodes = {
+  "links":[],
+  "nodes":[]
+}
+
+for (var i=0; i < originalNodes["nodes"].length; i++) {
+  newNodes["nodes"][i] = {};
+  newNodes["nodes"][i]["data"] = {};
+  newNodes["nodes"][i]["id"] = `"${i}"`;
+  newNodes["nodes"][i]["data"]["label"] = originalNodes["nodes"][i]["DialogText"];
+  newNodes["nodes"][i]["position"] = { x: 200, y: (i * 150) };
+  // console.log(newNodes["nodes"][i]);
+}
     
-// console.log(newNodes["nodes"]);
+console.log(newNodes["nodes"]);
 
 // for (var i=0; i < json["interviewerDialogs"].length; i++) {
 //   var obj = {};
